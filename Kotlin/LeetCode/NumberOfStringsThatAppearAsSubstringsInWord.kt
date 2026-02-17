2619c077fb0481d80bc8a5d244db347a6cc2cdf1
Artem Taraskin <Artem.Taraskin@ah.nl>
2026-02-17 17:24:07 +0100

    [LeetCode][Java][Kotlin] 1967. Number of Strings That Appear as Substrings in Word

---
+++ Kotlin/LeetCode/NumberOfStringsThatAppearAsSubstringsInWord.kt
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/number-of-strings-that-appear-as-substrings-in-word/description/
+// 1967. Number of Strings That Appear as Substrings in Word
+// EASY
+
+class Solution {
+    fun numOfStrings(patterns: Array<String>, word: String): Int {
+        var count = 0
+        for (pattern in patterns) {
+            if (word.contains(pattern)) count++
+        }
+        return count
+    }
+}
